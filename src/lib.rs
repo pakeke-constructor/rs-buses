6644b7a1be84627ec945ff7aad61eeedf4b90841
Oli <OliverWDGarrett@gmail.com>
2023-09-01 20:59:13 +1200

    cool stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,74 @@
 
+use std::vec::Vec;
+use std::option::Option;
 
 
 
 struct Event<T> {
-    callbacks: Vec<fn(T) -> ()>
-}
-
-trait Call {
-    fn call() -> ();
-}
-
-
-impl Call for Event<T> {
-    fn
+    name: String,
+    description: String,
+    callbacks: Vec<fn(&T) -> ()>
 }
 
 
+impl<T> Event<T> {
+    pub fn call(&self, data: &T) {
+        for func in self.callbacks.iter() {
+            func(data);
+        }
+    }
 
-
-pub fn call(event: Event, data: T) {
-    let funcs = 
+    pub fn on(&mut self, callback: fn(&T) -> ()) {
+        self.callbacks.push(callback);
+    }
 }
 
+
+
+
+struct Question<T, A> {
+    name: String,
+    description: String,
+    answers: Vec<fn(&T) -> A>,
+    reducer: fn(A,A) -> A
+}
+
+
+
+impl<T,A> Question<T, A> {
+    pub fn ask(&self, data: &T) -> Option<A> {
+        if self.answers.len() <= 0 {
+            return Option::None;
+        }
+
+        let ans_func = self.answers.get(0).unwrap();
+        let mut ret = ans_func(data);
+
+        for answer in self.answers.iter() {
+            let ans = answer(data);
+            ret = (self.reducer)(ret, ans);
+        }
+
+        return Option::Some(ret);
+    }
+
+    pub fn answer(&mut self, answer: fn(&T) -> A) {
+        self.answers.push(answer);
+    }
+}
+
+
+
+pub fn newEvent(name: String, description: String) -> Event {
+    let vec = Vec::new();
+    let mut ev: Event = Event {
+        name: name,
+        description: description,
+        callbacks: vec
+    };
+
+    ev
+}
 
 
 
@@ -29,12 +76,16 @@ pub fn call(event: Event, data: T) {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    #[test]
+    fn test_events() {
+
+        let result = 4;
+        assert_eq!(result, 4);
+    }
+
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_questions() {
     }
 }
 
